e89941d4707e678ba06531e23052d565ae79adbb
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-16 20:32:46 +0700

    Search,Siderbar,Component

--- src/components/layouts/search/search.tsx
+++ src/components/layouts/search/search.tsx
@@ -1,9 +1,152 @@
-import React from 'react'
+import { UserAddOutlined, UsergroupAddOutlined } from "@ant-design/icons";
+import { Input, message } from "antd";
 
-const search = () => {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { IFriends } from "../../models/friends";
+
+const Search: React.FC = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [friends, setFriends] = useState<IFriends[] | null>(null);
+  const [originalFriends, setOriginalFriends] = useState<IFriends[] | null>(
+    null
+  );
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // useEffect(() => {
+  //   const getList = async () => {
+  //     try {
+  //       const token = localStorage.getItem("token");
+  //       const response = await axios.get("http://localhost:8000/friend/list", {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       });
+  //       setFriends(response.data.users);
+  //       setOriginalFriends(response.data.users);
+  //       // console.log(response.data.users);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   getList();
+  // }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://localhost:8000/conversation/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        const conversations = response.data.conversations;
+        if (!conversations) {
+          throw new Error("Danh sách cuộc trò chuyện không tồn tại.");
+        }
+        const listConversations: IFriends[] = conversations.map(
+          (conversations: any) => {
+            return {
+              _id: conversations._id,
+              userId: conversations.userId,
+              avatar: conversations.avatar,
+              name: conversations.name,
+              lastMessage: conversations.lastMessage,
+              type: conversations.type,
+              conversationId: conversations.conversationId,
+            };
+          }
+        );
+        setFriends(listConversations);
+        setOriginalFriends(listConversations);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching conversations:", error);
+        setError(error.message || "Không thể tải danh sách cuộc trò chuyện");
+        setLoading(false);
+      });
+  }, []);
+
+  const handleSearch = (e: any) => {
+    if (e == "") {
+      setFriends(originalFriends);
+      setLoading(false);
+      return;
+    }
+    setLoading(true);
+    const token = localStorage.getItem("token");
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    axios
+
+      .get(`http://localhost:8000/friend/find/${phoneNumber}`, config)
+      .then((response) => {
+        const data = response.data;
+
+        if (data.friend) {
+          setFriends([data.friend]);
+        } else {
+          message.info("Friend not found.");
+        }
+      })
+
+      .catch((error) => {
+        console.error("Error searching friend:", error);
+        message.error("Failed to search for friend.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
-    <div>search</div>
-  )
-}
+    <div
+      style={{
+        flexDirection: "row",
+        height: 70,
+        marginLeft: 100,
+        marginTop: 0,
+        position: "fixed",
+        borderRight: "0.5px solid #ededee",
+      }}
+    >
+      <Input.Search
+        value={phoneNumber}
+        placeholder="Tìm kiếm"
+        // onSearch={handleSearch}
+        onSearch={(e) => {
+          handleSearch(e);
+        }}
+        allowClear
+        loading={loading}
+        style={{ width: 200, color: "red", marginLeft: 10, marginTop: 10 }}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
 
-export default search
+      <UserAddOutlined
+        style={{
+          marginTop: "10px",
+          marginLeft: 5,
+          width: 30,
+          height: 30,
+          color: "gray",
+        }}
+      />
+      <UsergroupAddOutlined style={{ width: 40, height: 50, color: "gray" }} />
+      
+    </div>
+  );
+};
+
+export default Search;
--- src/components/layouts/siderbar/siderbar.tsx
+++ src/components/layouts/siderbar/siderbar.tsx
@@ -1,9 +1,174 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const siderbar = () => {
+import { Menu, Avatar } from "antd";
+import {
+  CheckSquareOutlined,
+  CloudOutlined,
+  ContactsOutlined,
+  MessageOutlined,
+  SettingOutlined,
+  ShopOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import SubMenu from "antd/es/menu/SubMenu";
+
+import axios from "axios";
+
+const Siderbar: React.FC = () => {
+  const [isOpenModalInfo, setIsOpenModalInfo] = useState(false);
+  const [user, setUser] = useState<any>(null);
+  const[token,setToken]=useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    
+    if (token) {
+      axios
+        .get("http://localhost:8000/auth/user", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          setUser(response.data.user); 
+          
+        })
+        .catch((error) => {
+          console.error("Error getting user:", error);
+        });
+    }
+  }, []);
+ 
+  const handleLogout = async () => {
+    try {
+      localStorage.removeItem("token");
+      if (setToken) { 
+        setToken(null);
+      } else {
+        console.warn("setToken function not found. State might not be cleared.");
+      }
+      window.location.href = "/login"; 
+    } catch (error) {
+      console.error("Error during logout:", error);
+
+    }
+  };
+  const openModalInfo = () => {
+    setIsOpenModalInfo(true);
+  };
+  const handleOk = () => {
+    setIsOpenModalInfo(false);
+  };
+
+  const handleCancel = () => {
+    setIsOpenModalInfo(false);
+  };
   return (
-    <div>siderbar</div>
-  )
-}
+    <div
+      style={{
+        width: "100px",
+        background: "#3a98ff",
+        height: "750px",
+        marginLeft: -8,
+        marginTop: -30,
+        position: "fixed",
+      }}
+    >
+      <div>
+      
+        <Menu
+          style={{
+            background: "#3a98ff",
+            justifyContent: "center",
+            marginTop: 40,
+          }}
+        >
+          <SubMenu
+            key="information"
+            icon={
+              <Avatar
+                src={user ? user.avatar : ""}
+                size={40}
+                style={{ marginLeft: 8, marginTop: 1, marginBottom: 10 }}
+              >
+                {<UserOutlined style={{ fontSize: "30px", color: "#fff" }} />}
+              </Avatar>
+            }
+          >
+            <div>
+              <h4>{user ? user.name : "Sai..."}</h4>
+            </div>
+            <hr />
+            <Menu.Item
+              key="hs"
+              onClick={() => {
+                openModalInfo();
+              }}
+            >
+              Hồ sơ của bạn
+            </Menu.Item>
+           
+            <Menu.Item key="cd">Cài đặt</Menu.Item>
+            <Menu.Item key="dx" onClick={handleLogout}>Đăng xuất</Menu.Item>
+          </SubMenu>
 
-export default siderbar
+          <Menu.Item key={2}>
+            <Link to="/message">
+              {" "}
+              <MessageOutlined
+                style={{
+                  fontSize: "25px",
+                  color: "#fff",
+                  margin: 10,
+                  marginTop: 10,
+                }}
+              />
+            </Link>
+          </Menu.Item>
+
+          <Menu.Item key={3}>
+            <Link to="/contact">
+              <ContactsOutlined
+                style={{ fontSize: "25px", color: "#fff", margin: 10 }}
+              />
+            </Link>
+          </Menu.Item>
+          <Menu.Item key={4}>
+            <CheckSquareOutlined
+              style={{ fontSize: "25px", color: "#fff", margin: 10 }}
+            />
+          </Menu.Item>
+          <Menu.Item key={5}>
+            <VideoCameraOutlined
+              style={{ fontSize: "25px", color: "#fff", margin: 10 }}
+            />
+          </Menu.Item>
+
+          <div style={{ height: 320 }}></div>
+
+          <Menu.Item key={6}>
+            <CloudOutlined
+              style={{ fontSize: "25px", color: "#fff", margin: 10 }}
+            />
+          </Menu.Item>
+
+          <Menu.Item key={7}>
+            <ShopOutlined
+              style={{ fontSize: "25px", color: "#fff", margin: 10 }}
+            />
+          </Menu.Item>
+          <Menu.Item key={8}>
+            <SettingOutlined
+              style={{ fontSize: "25px", color: "#fff", margin: 10 }}
+            />
+          </Menu.Item>
+        </Menu>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Siderbar;
