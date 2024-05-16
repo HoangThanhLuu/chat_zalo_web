32aa6a3bbbb54a60b823bb2a635e9c27f48e1e76
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-16 20:27:30 +0700

    friends.ts,user.ts

---
+++ src/components/models/friends.ts
@@ -0,0 +1,13 @@
+export interface IFriends {
+    _id: string | null;
+    avatar: string;
+    name: string;
+    message: string;
+    checkIsFriends: boolean;
+    chatName: string;
+    type: string;
+    conversationId: string;
+    lastMessages:string;
+  
+  }
+  
