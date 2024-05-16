b09f7d60ad23b6a6ac5dfc8fc8a1604da6320ccd
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-16 22:50:37 +0700

    Menu

--- src/pages/ContactPage/Menu.tsx
+++ src/pages/ContactPage/Menu.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
+import { MailOutlined, TeamOutlined, UserAddOutlined } from "@ant-design/icons";
 
-const Menu = () => {
+const MenuItem: React.FC = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <Menu
+    >
+      <Menu.Item key={1}>
+        <Link to="/listFriends">
+          <UserAddOutlined style={{ fontSize: "25px", color: "gray", margin: 10, marginLeft: 0 }} />
+          Danh sách bạn bè
+        </Link>
+      </Menu.Item>
+      <Menu.Item key={2}>
+        <Link to="/listGroups">
+          <TeamOutlined style={{ fontSize: "25px", color: "gray", margin: 10, marginLeft: 0 }} />
+          Danh sách nhóm
+        </Link>
+      </Menu.Item>
+      <Menu.Item key={3}>
+        <Link to="/friendsRequest">
+          <MailOutlined style={{ fontSize: "25px", color: "gray", margin: 10, marginLeft: 0 }} />
+          Lời mời kết bạn
+        </Link>
+      </Menu.Item>
+    </Menu>
+  );
+};
 
-export default Menu
+export default MenuItem;
