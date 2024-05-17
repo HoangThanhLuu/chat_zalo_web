3a1c071f9caf504ca05d86f130ebac1b62ead01f
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-17 10:29:51 +0700

    ModalCreateGroupChat tạo nhóm chat

--- src/components/modals/ModalCreateGroupChat.tsx
+++ src/components/modals/ModalCreateGroupChat.tsx
@@ -2,8 +2,67 @@ import React from 'react'
 
 const ModalCreateGroupChat = () => {
   return (
-    <div>ModalCreateGroupChat</div>
-  )
-}
+    <Modal
+      title="Tạo nhóm"
+      visible={open}
+     
+      okText="Tạo nhóm"
+    >
+      <div>
+        <Row>
+          <Col span={2}>
+          
+          <Avatar
+            style={{
+              height: 30,
+              width: 30,
+              marginLeft: 5,
+              backgroundColor: "gray",
+              border: "none",
+            }}
+            // src={?.avatar}
+          />
+         
+          </Col>
+          <Col span={22}>
+            <Input   placeholder="Nhập tên nhóm"></Input>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+            <Input.Search
+            
+              placeholder="Nhập số điện thoại"
+              style={{
+                display: "flex",
+                flexDirection: "row-reverse",
+                marginTop: 10,
+                borderRadius: 20,
+              }}
+            
+            
+              allowClear
+             
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>
+           
+              <List
+                style={{
+                  marginLeft: 0,
+                }}
+                itemLayout="horizontal"
+                )}
+              />
+          
+          </Col>
+        </Row>
+      </div>
+    </Modal>
+  );
+};
+
 
 export default ModalCreateGroupChat
