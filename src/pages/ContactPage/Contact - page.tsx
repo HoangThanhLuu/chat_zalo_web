2f2750571106e206c09ad2a0c0fcd69b7b536f81
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-18 13:17:40 +0700

    ContactPage tìm kiếm

--- src/pages/ContactPage/Contact - page.tsx
+++ src/pages/ContactPage/Contact - page.tsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import Component from "../../components/layouts/components/components";
+import { Col, Input, Row, message } from "antd";
+import MenuItem from "./Menu";
+import Siderbar from "../../components/layouts/siderbar/siderbar";
+import Search from "../../components/layouts/search/search";
+import { useState } from "react";
+import { IFriends } from "../../components/models/friends";
+import axios from "axios";
+import { UserAddOutlined, UsergroupAddOutlined } from "@ant-design/icons";
+import ListDataFriends from "../MessagePage/ListDataFriends";
 
-const ContactPage = () => {
+const ContactPage: React.FC = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [friends, setFriends] = useState<IFriends[] | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = (e: any) => {
+   
+  }; 
+
   return (
-    <div>ContactPage</div>
-  )
-}
-
-export default ContactPage
+    <Row>
+      <Col span={1} style={{ position: "fixed" }}>
+        <Siderbar />
+      </Col>
+      <Col
+        style={{
+          flexDirection: "row",
+          height: 70,
+          marginLeft: 100,
+          marginTop: 0,
+          position: "fixed",
+          borderRight: "0.5px solid #ededee",
+        }}
+        span={7}
+      > 
+        <Input.Search
+          value={phoneNumber}
+          placeholder="Tìm kiếm"
+          onSearch={(e) => {
+            if (e == "") {
+              setFriends(null);
+            } else {
+              setPhoneNumber(e);
+              handleSearch(e);
+            }
+          }}
+          loading={loading}
+          allowClear
+          style={{ width: 200, color: "red", marginLeft: 10, marginTop: 10 }}
+          onChange={(e) => {
+            // if (e.target.value == null) {
+            // }
+            setPhoneNumber(e.target.value);
+          }}
+        />
+        <UserAddOutlined
+          style={{
+            marginTop: "10px",
+            marginLeft: 5,
+            width: 30,
+            height: 30,
+            color: "gray",
+          }}
+        />
+        <UsergroupAddOutlined
+          style={{ width: 40, height: 50, color: "gray" }}
+        />
+        {friends ? <ListDataFriends users={friends} /> : <MenuItem />}
+      </Col>
+    </Row>
+  );
+};
+export default ContactPage;
