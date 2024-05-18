2ec7d901bca6caa5a7f649b0d3b85afa33cba198
HoangThanhLuu <luuhoang06102002@gmail.com>
2024-05-18 12:21:08 +0700

    createGroupChat tạo group chat

--- src/components/modals/ModalCreateGroupChat.tsx
+++ src/components/modals/ModalCreateGroupChat.tsx
@@ -1,17 +1,107 @@
-import React from 'react'
+import {
+  Modal,
+  Col,
+  Row,
+  Input,
+  ModalProps,
+  Avatar,
+  Upload,
+  List,
+  message,
+  Checkbox,
+} from "antd";
 
-const ModalCreateGroupChat = () => {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { IFriends } from "../models/friends";
+
+const ModalCreateGroupChat: React.FC<ModalProps> = ({ open, onCancel }) => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [friends, setFriends] = useState<IFriends[] | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [chatName, setChatName] = useState("");
+  const [memberIds, setMemberIds] = useState([]);
+  const [file, setFile] = useState<any>(null);
+  const token = localStorage.getItem("token");
+
+
+  useEffect(() => {
+    const getList = async () => {
+      try {
+         ;
+        const token = localStorage.getItem("token");
+        const response = await axios.get("http://localhost:8000/friend/list", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setFriends(response.data.friends);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getList();
+  }, []);
+  const handleSearch = (e: any) => {
+    
+  };
+  const dummyRequest = async ({ file, onSuccess }) => {
+    setTimeout(() => {
+      onSuccess("ok");
+    }, 0);
+  };
+  const uploadImage = {
+   
+  };
+  const CreateGroupChat = async () => {
+    const formData = new FormData();
+    formData.append("chatName", chatName);
+    for(const item of memberIds){
+       formData.append("memberIds", item);
+    }
+    console.log(formData);
+
+    if (file) {
+      formData.append("image", file.originFileObj);
+    }
+     ;
+    axios
+      .post("http://localhost:8000/conversation/group", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+         ;
+        console.log("Group chat created:", response.data);
+        message.success("Group chat created successfully!");
+      
+      })
+      .catch((error) => {
+         ;
+        console.error("Error creating group chat:", error);
+        message.error("Failed to create group chat.");
+      });
+  };
+
+  const handleCheckboxChange = (itemId: string): void => {
+   
+  };
+
   return (
     <Modal
       title="Tạo nhóm"
       visible={open}
-     
+      onCancel={onCancel}
+      onOk={CreateGroupChat}
+      cancelText="Hủy"
       okText="Tạo nhóm"
     >
       <div>
         <Row>
           <Col span={2}>
-          
+          <Upload {...uploadImage}>
           <Avatar
             style={{
               height: 30,
@@ -22,16 +112,16 @@ const ModalCreateGroupChat = () => {
             }}
             // src={?.avatar}
           />
-         
+          </Upload>
           </Col>
           <Col span={22}>
-            <Input   placeholder="Nhập tên nhóm"></Input>
+            <Input value={chatName} onChange={handleChatNameChange} placeholder="Nhập tên nhóm"></Input>
           </Col>
         </Row>
         <Row>
           <Col span={24}>
             <Input.Search
-            
+              value={phoneNumber}
               placeholder="Nhập số điện thoại"
               style={{
                 display: "flex",
@@ -39,24 +129,45 @@ const ModalCreateGroupChat = () => {
                 marginTop: 10,
                 borderRadius: 20,
               }}
-            
-            
+              onSearch={(e) => {
+                if (e == "") {
+                  setFriends(null);
+                } else {
+                  setPhoneNumber(e);
+                  handleSearch(e);
+                }
+              }}
+              loading={loading}
               allowClear
-             
+              onChange={(e) => {
+                setPhoneNumber(e.target.value);
+              }}
             />
           </Col>
         </Row>
         <Row>
           <Col span={24}>
-           
+            {friends !== null && (
               <List
                 style={{
                   marginLeft: 0,
                 }}
                 itemLayout="horizontal"
+                dataSource={friends}
+                renderItem={(item) => (
+                  <List.Item key={item._id} style={{ marginLeft: 40 }}>
+                    <Checkbox
+                      checked={memberIds.includes(item._id)}
+                      onChange={() => handleCheckboxChange(item._id)}
+                    />
+                    <List.Item.Meta
+                      avatar={<Avatar src={item.avatar} />}
+                      title={item.name}
+                    />
+                  </List.Item>
                 )}
               />
-          
+            )}
           </Col>
         </Row>
       </div>
@@ -64,5 +175,4 @@ const ModalCreateGroupChat = () => {
   );
 };
 
-
-export default ModalCreateGroupChat
+export default ModalCreateGroupChat;
